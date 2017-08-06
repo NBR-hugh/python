e884a8c4ed7d223bb7ddc16cd83b9de4399b7e54
birong ni <574168029@qq.com>
2017-08-06 14:23:07 +0800

    ex16 BE00

--- books/learn-python-the-hard-way/ex_1/ex16.py
+++ books/learn-python-the-hard-way/ex_1/ex16.py
@@ -1,41 +1,37 @@
 # -*- coding: utf-8 -*-
 
-from sys import argv      # call system for the moudul:argv
+#BE 00
 
-script,filename = argv     # set two arguments for argv
+from  sys import argv
 
-print "We're going to erase %r." % filename    # output string
-print "If you don't want that, hit CTRL-C (ˆC)."  # output string
-print "If you do want that, hit RETURN."   # output string
+script,filename = argv
 
-raw_input("?")            # get the response of user
+print "We're going to erase %r." % filename
+print "If you don't want that,hit CTRL-C(ˆC)"
+print "If you do want that, hit RETURN."
 
-print "Opening the file..."  # output string
-target = open(filename,'w')    # variaty target: open file and ready to write
+raw_input("?")
 
-print "Truncating the file. Goodbye!"  # output string
-target.truncate()             # delete all content of file pointed by target
+print "Open the file..."
+target = open(filename,'w')
 
-print "Now I'm going to ask you for three lines." # output string
+print "Truncating the file.Goodbye!"
+target.truncate()
 
-line1 = raw_input("line 1:")     # deliver response of user to variaty line1/2/3
-line2 = raw_input("line 2:")
-line3 = raw_input("line 3:")
+print "Now I'm going to ask you for three lines."
 
-print "I'm going to write these to the file."    # output string
+line1 = raw_input("line1:")
+line2 = raw_input("line2:")
+line3 = raw_input("line3:")
 
-#target.write(line1)          # deliver variaty line1/2/3 and \n to file
-#target.write("\n")
-#target.write(line2)
-#target.write("\n")
-#target.write(line3)
-#target.write("\n")
+print "I'm going to write these to the file."
 
-# n = line1 + "\n" + line2 + "\n" + line3 + "\n"
-# target.write(n)
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
 
-n = "%r\n%r\n%r\n" % (line1,line2,line3)
-target.write(n)
-
-print "And finally,we close it."  # output string
-target.close()         # cloed and save target
+print "And finally,We close it."
+target.close()
